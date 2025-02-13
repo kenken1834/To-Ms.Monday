b679623eeb4a297d023a922d599a3cfc16fd59f0
kenken1834 <ventilacionken3@gmail.com>
2025-02-13 14:48:10 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'BE MY VALENTINE?'.split('')
+const text = 'MY LAST GIFT'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
