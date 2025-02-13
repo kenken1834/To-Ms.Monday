ed32490cd9de8757c12a57d57db5939124d38080
kenken1834 <ventilacionken3@gmail.com>
2025-02-13 14:48:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LIKE YOU, SHANNA').split('')
+    const titles = ('HAPPY VALENTINES, SHANNA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
